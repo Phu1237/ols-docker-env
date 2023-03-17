b93657bce1b9cab2b386724ccff033d6d82bf533
Phu1237 <phu1237@gmail.com>
2023-03-17 10:36:08 +0700

    add clean command

--- bin/domain.sh
+++ bin/domain.sh
@@ -16,6 +16,8 @@ help_message(){
     echo "${EPACE}${EPACE}Example: domain.sh -D example.com, will delete the domain from Listener."
     echow "-m, --make [domain_name]"
     echo "${EPACE}${EPACE}Example: domain.sh -M example.com, will create diretories for domain from Listener."
+    echow "-c, --clean [domain_name]"
+    echo "${EPACE}${EPACE}Example: domain.sh -C example.com, will remove diretories and config file of domain from Listener."
     echow '-H, --help'
     echo "${EPACE}${EPACE}Display help and exit."    
 }
@@ -47,6 +49,11 @@ make_dir(){
     docker compose exec ${CONT_NAME} su -s /bin/bash lsadm -c "cd /usr/local/lsws/conf && mkdir -p sites/${1}/{html,logs,certs,conf} && chown lsadm:lsadm sites/${1}/conf"
 }
 
+clean(){
+    check_input ${1}
+    docker compose exec ${CONT_NAME} su -s /bin/bash lsadm -c "cd /usr/local/lsws/conf && rm -rf sites/${1} && rm -rf /usr/local/lsws/conf/${1}"
+}
+
 check_input ${1}
 while [ ! -z "${1}" ]; do
     case ${1} in
@@ -59,8 +66,11 @@ while [ ! -z "${1}" ]; do
         -[dD] | -del | --del | --delete) shift
             del_domain ${1}
             ;;          
-        -[mM] | -make | --make | --make) shift
+        -[mM] | -make | --make) shift
             make_dir ${1}
+            ;;          
+        -[cC] | -clean | --clean) shift
+            clean ${1}
             ;;          
         *) 
             help_message
